a1f071bfa81f795aff5c8503009e681113749b7c
Udipt Srivastava <124235867+Udipt13@users.noreply.github.com>
2023-04-21 00:52:09 +0530

    up

--- airbyte-integrations/connectors/source-data-pull/source_data_pull/source.py
+++ airbyte-integrations/connectors/source-data-pull/source_data_pull/source.py
@@ -1,38 +1,7 @@
-#
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
-#
-
-
-from abc import ABC
-from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 from airbyte_cdk.sources.streams.http.auth import NoAuth
-import requests
-from airbyte_cdk.sources import AbstractSource
-from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.sources.streams.http import HttpStream
-from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
 
-# Source
-class SourceDataPull(AbstractSource):
-    def check_connection(self, logger, config) -> Tuple[bool, any]:
-        
-        accepted_currencies = {"USD", "JPY", "BGN", "CZK", "DKK"}  # assume these are the only allowed currencies
-        input_currency = config['base']
-        if input_currency not in accepted_currencies:
-            return False, f"Input currency {input_currency} is invalid. Please input one of the following currencies: {accepted_currencies}"
-        else:
-            return True, None
 
-    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
-        """
-        TODO: Replace the streams below with your own streams.
 
-        :param config: A Mapping of the user input configuration as defined in the connector spec.
-        """
-        # TODO remove the authenticator if not required.
-        auth = TokenAuthenticator(token="api_key")  # Oauth2Authenticator is also available if you need oauth support
-        return [Customers(authenticator=auth), Employees(authenticator=auth)]
-        
 class ExchangeRates(HttpStream):
     url_base = "https://api.apilayer.com/exchangerates_data/"
 
@@ -59,8 +28,6 @@ class ExchangeRates(HttpStream):
         next_page_token: Mapping[str, Any] = None,
     ) -> Iterable[Mapping]:
         return None  # TODO
-
-
 
 class SourcePythonHttpTutorial(AbstractSource):
 
