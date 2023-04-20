1ab71676ff37ea7446764c97009fe1870c70eefb
Udipt Srivastava <124235867+Udipt13@users.noreply.github.com>
2023-04-20 23:24:23 +0530

    commit

--- airbyte-integrations/connectors/source-data-pull/source_data_pull/source.py
+++ airbyte-integrations/connectors/source-data-pull/source_data_pull/source.py
@@ -5,7 +5,7 @@
 
 from abc import ABC
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
-
+from airbyte_cdk.sources.streams.http.auth import NoAuth
 import requests
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
@@ -200,3 +200,44 @@ class SourceDataPull(AbstractSource):
         # TODO remove the authenticator if not required.
         auth = TokenAuthenticator(token="api_key")  # Oauth2Authenticator is also available if you need oauth support
         return [Customers(authenticator=auth), Employees(authenticator=auth)]
+        
+class ExchangeRates(HttpStream):
+    url_base = "https://api.apilayer.com/exchangerates_data/"
+
+    # Set this as a noop.
+    primary_key = None
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        # The API does not offer pagination, so we return None to indicate there are no more pages in the response
+        return None
+
+    def path(
+        self, 
+        stream_state: Mapping[str, Any] = None, 
+        stream_slice: Mapping[str, Any] = None, 
+        next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return ""  # TODO
+
+    def parse_response(
+        self,
+        response: requests.Response,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        return None  # TODO
+
+
+
+class SourcePythonHttpTutorial(AbstractSource):
+
+    def check_connection(self, logger, config) -> Tuple[bool, any]:
+        ...
+
+    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+        # NoAuth just means there is no authentication required for this API and is included for completeness.
+        # Skip passing an authenticator if no authentication is required.
+        # Other authenticators are available for API token-based auth and Oauth2. 
+        auth = NoAuth()  
+        return [ExchangeRates(authenticator=auth)]
