346ac5fb40b78465b60e9ac3f089a82f4796ac1b
Udipt Srivastava <124235867+Udipt13@users.noreply.github.com>
2023-04-21 14:11:14 +0530

    ok

--- airbyte-integrations/connectors/source-data-pull/source_data_pull/source.py
+++ airbyte-integrations/connectors/source-data-pull/source_data_pull/source.py
@@ -7,6 +7,7 @@ from abc import ABC
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 from airbyte_cdk.sources.streams.http.auth import NoAuth
 import requests
+import csv
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
@@ -205,71 +206,139 @@ There are additional required TODOs in the files within the integration_tests fo
 #         # TODO remove the authenticator if not required.
 #         auth = TokenAuthenticator(token="api_key")  # Oauth2Authenticator is also available if you need oauth support
 #         return [Customers(authenticator=auth), Employees(authenticator=auth)]
-        
-class ExchangeRates(HttpStream): # same name as given in schema
-    url_base = "https://api.apilayer.com/exchangerates_data/"
+
+class CsvData(HttpStream): # same name as given in schema
+    url_base = "https://storage.googleapis.com/covid19-open-data/v2/latest/epidemiology.csv"
 
     # Set this as a noop.
     primary_key = None
 
-    def __init__(self, config: Mapping[str, Any], **kwargs):
-        super().__init__()
-        self.base = config['base']
-        self.apikey = config['apikey']
+    # def __init__(self, config: Mapping[str, Any], **kwargs):
+    #     super().__init__()
+    #     self.base = config['base']
+    #     self.apikey = config['apikey']
 
-    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        # The API does not offer pagination, so we return None to indicate there are no more pages in the response
-        return None
+    # def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+    #     # The API does not offer pagination, so we return None to indicate there are no more pages in the response
+    #     return None
 
-    def path(
-        self, 
-        stream_state: Mapping[str, Any] = None, 
-        stream_slice: Mapping[str, Any] = None, 
-        next_page_token: Mapping[str, Any] = None
-    ) -> str:
-        # The "/latest" path gives us the latest currency exchange rates
-        return "latest" 
+    # def path(
+    #     self, 
+    #     stream_state: Mapping[str, Any] = None, 
+    #     stream_slice: Mapping[str, Any] = None, 
+    #     next_page_token: Mapping[str, Any] = None
+    # ) -> str:
+    #     # The "/latest" path gives us the latest currency exchange rates
+    #     return "latest" 
 
-    def request_headers(
-        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-    ) -> Mapping[str, Any]:
-        # The api requires that we include apikey as a header so we do that in this method
-        return {'apikey': self.apikey}
+    # def request_headers(
+    #     self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    # ) -> Mapping[str, Any]:
+    #     # The api requires that we include apikey as a header so we do that in this method
+    #     return {'apikey': self.apikey}
 
-    def request_params(
-            self,
-            stream_state: Mapping[str, Any],
-            stream_slice: Mapping[str, Any] = None,
-            next_page_token: Mapping[str, Any] = None,
-    ) -> MutableMapping[str, Any]:
-        # The api requires that we include the base currency as a query param so we do that in this method
-        return {'base': self.base}
+    # def request_params(
+    #         self,
+    #         stream_state: Mapping[str, Any],
+    #         stream_slice: Mapping[str, Any] = None,
+    #         next_page_token: Mapping[str, Any] = None,
+    # ) -> MutableMapping[str, Any]:
+    #     # The api requires that we include the base currency as a query param so we do that in this method
+    #     return {'base': self.base}
 
-    def parse_response(
-            self,
-            response: requests.Response,
-            stream_state: Mapping[str, Any],
-            stream_slice: Mapping[str, Any] = None,
-            next_page_token: Mapping[str, Any] = None,
-    ) -> Iterable[Mapping]:
+    def parse_response(self):
+
         # The response is a simple JSON whose schema matches our stream's schema exactly, 
         # so we just return a list containing the response
-        return [response.json()]
+        response = requests.get(url)
+        decoded_content = response.content.decode('utf-8')
+        csvreader = csv.reader(decoded_content.splitlines(), delimiter=',')
+        #header = next(csvreader)  # skip the header row if it exists
+        data = []
+        for row in csvreader:
+            data.append(row)
+        return [data]
     
-    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
-        auth = NoAuth()
-        return [ExchangeRates(authenticator=auth, config=config)]
-
-
+    def streams(self):   
+        return [CsvData()]
 
 class SourceDataPull(AbstractSource): #same name as conector 
-
-    def check_connection(self, logger, config) -> Tuple[bool, any]:
         ...
 
-    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+    def streams(self):
         # NoAuth just means there is no authentication required for this API and is included for completeness.
         # Skip passing an authenticator if no authentication is required.
         # Other authenticators are available for API token-based auth and Oauth2. 
-        auth = NoAuth()  
-        return [ExchangeRates(authenticator=auth,config=config)]
+      
+        return [CsvData()]
+
+        
+# class ExchangeRates(HttpStream): # same name as given in schema
+#     url_base = "https://api.apilayer.com/exchangerates_data/"
+
+#     # Set this as a noop.
+#     primary_key = None
+
+#     def __init__(self, config: Mapping[str, Any], **kwargs):
+#         super().__init__()
+#         self.base = config['base']
+#         self.apikey = config['apikey']
+
+#     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+#         # The API does not offer pagination, so we return None to indicate there are no more pages in the response
+#         return None
+
+#     def path(
+#         self, 
+#         stream_state: Mapping[str, Any] = None, 
+#         stream_slice: Mapping[str, Any] = None, 
+#         next_page_token: Mapping[str, Any] = None
+#     ) -> str:
+#         # The "/latest" path gives us the latest currency exchange rates
+#         return "latest" 
+
+#     def request_headers(
+#         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+#     ) -> Mapping[str, Any]:
+#         # The api requires that we include apikey as a header so we do that in this method
+#         return {'apikey': self.apikey}
+
+#     def request_params(
+#             self,
+#             stream_state: Mapping[str, Any],
+#             stream_slice: Mapping[str, Any] = None,
+#             next_page_token: Mapping[str, Any] = None,
+#     ) -> MutableMapping[str, Any]:
+#         # The api requires that we include the base currency as a query param so we do that in this method
+#         return {'base': self.base}
+
+#     def parse_response(
+#             self,
+#             response: requests.Response,
+#             stream_state: Mapping[str, Any],
+#             stream_slice: Mapping[str, Any] = None,
+#             next_page_token: Mapping[str, Any] = None,
+#     ) -> Iterable[Mapping]:
+#         # The response is a simple JSON whose schema matches our stream's schema exactly, 
+#         # so we just return a list containing the response
+#         return [response.json()]
+    
+#     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+#         auth = NoAuth()
+#         return [ExchangeRates(authenticator=auth, config=config)]
+
+
+
+# class SourceDataPull(AbstractSource): #same name as conector 
+
+#     def check_connection(self, logger, config) -> Tuple[bool, any]:
+#         ...
+
+#     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+#         # NoAuth just means there is no authentication required for this API and is included for completeness.
+#         # Skip passing an authenticator if no authentication is required.
+#         # Other authenticators are available for API token-based auth and Oauth2. 
+#         auth = NoAuth()  
+#         return [ExchangeRates(authenticator=auth,config=config)]
+
+
